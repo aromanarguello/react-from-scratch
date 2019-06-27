d510172b76b4e759b8255bf3679f9a6a58258563
Alejandro <alejandro.roman@ge.com>
2019-06-27 09:12:54 -0400

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    mode: 'development',
+    output: {
+        filename: './main.js'
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000,
+        watchContentBase: true,
+        progress: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: ['file-loader']
+            }
+        ]
+    }
+};
